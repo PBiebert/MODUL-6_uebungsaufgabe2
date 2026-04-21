5e735a7fa8e1477c1e4fde3e03773808c177fbcd
Philipp Biebert <philipp.biebert@gmail.com>
2026-04-21 13:07:20 +0200

    feat: Add models for student, professor, semester, course, and course description

--- university_app/models.py
+++ university_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    matriculation_Number = models.CharField(max_length=20, unique=True)
+
+
+class StudentId (models.Model):
+    student = models.OneToOneField(
+        Student, on_delete=models.CASCADE)
+
+
+class Professor (models.Model):
+    name = models.CharField(max_length=30)
+    field_of_expertise = models.CharField(max_length=30)
+
+
+class Semester (models.Model):
+    designation = models.CharField(max_length=30)
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=30)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+
+
+class CourseDescription (models.Model):
+    course_description = models.CharField(max_length=300)
+    course = models.OneToOneField(Course, on_delete=models.CASCADE)
