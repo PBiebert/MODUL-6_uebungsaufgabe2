16b1178480a6cecd6b1615f29c3ee6d30c80c00f
Philipp Biebert <philipp.biebert@gmail.com>
2026-04-21 14:03:02 +0200

    style: Format code for better readability in Course and CourseDescription models

--- university_app/models.py
+++ university_app/models.py
@@ -25,11 +25,14 @@ class Semester (models.Model):
 class Course(models.Model):
     course_name = models.CharField(max_length=30)
     professor = models.ForeignKey(
-        Professor, on_delete=models.SET_NULL, null=True, blank=True, related_name="courses")
-    students = models.ManyToManyField(Student, related_name="enrolled_courses")
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, related_name="semester_courses")
+        Professor, on_delete=models.SET_NULL, null=True, blank=True, related_name="course_instructor")
+    students = models.ManyToManyField(
+        Student, related_name="enrolled_in_courses")
+    semester = models.ForeignKey(
+        Semester, on_delete=models.CASCADE, related_name="semester_courses")
 
 
 class CourseDescription (models.Model):
     course_description = models.CharField(max_length=300)
-    course = models.OneToOneField(Course, on_delete=models.CASCADE, related_name="description")
+    course = models.OneToOneField(
+        Course, on_delete=models.CASCADE, related_name="description")
